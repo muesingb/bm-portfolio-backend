01858e21aceabdf49a5c4197436ab749f211d793
Barbara <muesingb@gmail.com>
2020-04-01 13:02:32 -0400

    updated seeds and included route for infoblock show page

--- app/controllers/infoblocks_controller.rb
+++ app/controllers/infoblocks_controller.rb
@@ -2,4 +2,9 @@ class InfoblocksController < ApplicationController
     def index
         render json: Infoblock.all
     end
+
+    def show
+        @infoblock = Infoblock.find(params[:id])
+        render json: {infoblock: @infoblock, infocards: @infoblock.infocards}
+    end
 end
