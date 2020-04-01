b9f1b643fbfb02185c57468940fa6d320f9e1fc7
Barbara <muesingb@gmail.com>
2020-04-01 13:09:48 -0400

    refined infoblock show page

--- app/controllers/infoblocks_controller.rb
+++ app/controllers/infoblocks_controller.rb
@@ -5,6 +5,6 @@ class InfoblocksController < ApplicationController
 
     def show
         @infoblock = Infoblock.find(params[:id])
-        render json: {infoblock: @infoblock, infocards: @infoblock.infocards}
+        render json: {infoblock: @infoblock, infocards: @infoblock.infocard_ids}
     end
 end
