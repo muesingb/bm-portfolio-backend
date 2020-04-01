c6c1e3a00184afba4fe2ff0fbc0b0625edfc9d42
Barbara <muesingb@gmail.com>
2020-04-01 12:12:29 -0400

    hid seedfiles in separate seeds directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Infoblock.create(name: "about")
-Infoblock.create(name: "projects")
-Infoblock.create(name: "blog")
-Infoblock.create(name: "contact")
-
-Infocard.create(infoblock_id: 1, text:"About me", img_url: "pic of me")
-
-Infocard.create(infoblock_id: 2, text:"project 1", img_url: "pic of project 1")
-Infocard.create(infoblock_id: 2, text:"project 2", img_url: "pic of project 2")
-Infocard.create(infoblock_id: 2, text:"project 3", img_url: "pic of project 3")
-Infocard.create(infoblock_id: 2, text:"project 4", img_url: "pic of project 4")
-
-Infocard.create(infoblock_id: 3, text:"blog 1", img_url: "pic of blog 1")
-Infocard.create(infoblock_id: 3, text:"blog 2", img_url: "pic of blog 2")
-Infocard.create(infoblock_id: 3, text:"blog 3", img_url: "pic of blog 3")
-Infocard.create(infoblock_id: 3, text:"blog 4", img_url: "pic of blog 4")
+Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
+    load seed
+  end
